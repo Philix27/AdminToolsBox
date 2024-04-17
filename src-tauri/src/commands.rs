a76150db357501a2073e30a2be78ccfdd8cf9c30
Philix27 <philixbob@gmail.com>
2024-04-17 19:03:39 +0100

    tauri: setup postgres commands

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -4,12 +4,12 @@ pub fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-pub fn create_portal(name: &str) -> String {
-    format!("Hello pope, {}!", name)
+pub fn get(host: &str, user_name: &str, password: &str) -> String {
+    format!("Hello pope, {}!", user_name)
 }
 
 #[tauri::command]
-pub fn connect_postgres(name: &str) -> String {
+pub fn create_portal(name: &str) -> String {
     format!("Hello pope, {}!", name)
 }
 
